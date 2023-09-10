b048d5c265ae473f23c1bb793893a88144f145a4
natalia_znamierowska <nat.znamierowska@gmail..com>
2023-09-11 00:00:57 +0200

    xd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <div className="header">
-      <i className="logo fa-solid fa-money-bill-transfer"></i>
       <h1 className="title">PRZELICZNIK WALUT</h1>
     </div>
   );
